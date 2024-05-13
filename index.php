d3b150cee4bfa1fef3e534f990dbc0033c992629
eugenieoh <32752761+eugenieoh@users.noreply.github.com>
2024-05-13 22:03:05 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+    error_reporting(0);
+    //FFU AMP SHOPIFY - ARISHEM
+
+    $domain = $_SERVER['HTTP_HOST'];
+	$request = $_SERVER['REQUEST_URI'];
+    $cookie = explode("-",$domain);
+	
+	if(isset($_GET['paise']) && $_GET['paise'] !== "") {
+			$nama = $_GET['paise'];
+			$gen1 = $cookie[0];
+			$gen2 = $cookie[1];
+			$gen3 = "com";
+			$action = explode(".",$cookie[2]);
+			$action_link = $action[0];
+			$domain_name = "fate.24cc.cc";
+			$urlzzz = "https://bot-untuk-semesta.fr/account/register/mktintan123";
+			include("template.disini");
+	} else {
+		include("test_error.txt");
+	}
+?>
